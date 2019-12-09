f96eea58816a9713b4f9e602bda120e1b749c69a
Pavel <korsun4@mail.ru>
2019-12-09 21:56:33 +0300

    change extention

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const handlebars = require('express-handlebars').create({defaultLayout: 'main'});;
+const handlebars = require('express-handlebars');
 const app = express();
 
+handlebars.create({defaultLayout: 'main'});
+
 app.disable('x-powered-by');
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+app.engine('.hbs', handlebars({extname: '.hbs'}));
+app.set('view engine', '.hbs');
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
 
@@ -20,13 +22,13 @@ app.get('/about', (req, res) => {
 
 app.use((req, res) => {
     res.status(404);
-    res.render('404');
+    res.render('errors/404', {layout: 'error'});
 });
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
     console.log(err.stack);
     res.status(500);
-    res.render('500');
+    res.render('errors/500', {layout: 'error'});
 });
 
 app.listen(app.get('port'), () => {
