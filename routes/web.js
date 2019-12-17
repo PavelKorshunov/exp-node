1fbdf66dcf0241c98554ef53c3dcae83a12b8f43
Pavel <korsun4@mail.ru>
2019-12-17 22:23:01 +0300

    create people

--- routes/web.js
+++ routes/web.js
@@ -3,15 +3,17 @@ const Users = require('../models/users');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    Users.find({}, function(err, users) {
-        if (err) return console.error(err);
-        console.log(users);
-    });
     res.render('home');
 });
 
 router.get('/about', (req, res, next) => {
-    res.render('about');
+    Users.find({}, function(err, users) {
+        if (err) return console.error(err);
+        let context = {
+            people: users 
+        };
+        res.render('about', context);
+    });
 });
 
 module.exports = router;
